dd5572dca948c3a7ff67e3b11eb74bf5a55be2fd
Yura Shalya <shalyayura@gmail.com>
2019-01-26 21:13:14 +0200

    Calc infredient Sum, adding the Build Coltrol Component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import burgers from './Burger.module.css';
 import BurgerIngrediens from './Burgeringredient/Burgeringredient'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => { 
           return <BurgerIngrediens key={igKey + i} type={igKey} />;
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+     console.log(transformedIngredients);
     return(
         <div className={burgers.Burger}> 
             <BurgerIngrediens type="bread-top"/>
