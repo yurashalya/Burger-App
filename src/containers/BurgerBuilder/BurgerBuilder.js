1b164dca9c71ff0cd2e54c4c9546f6ead25fda30
Yura Shalya <shalyayura@gmail.com>
2019-01-28 02:19:51 +0200

    Total Price view Modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,7 +94,8 @@ class BurgerBuilder extends Component {
                 <Modal show={this.state.purchasing} modalClosed ={this.purchaseCancelHandler}> 
                     <OrderSummary ingredients = {this.state.ingredients}
                     purchaseCancelled = {this.purchaseCancelHandler}
-                    purchaseContinued = {this.purchaseContinueHandler} />
+                    purchaseContinued = {this.purchaseContinueHandler}
+                    price = {this.state.totalPrice} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
