d218c7c59bd39ae71cb1dc92fa6473ccae9f7c7c
Yura Shalya <shalyayura@gmail.com>
2019-02-01 00:56:56 +0200

    Connecting Project on the FireBase and sending a Post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -77,7 +79,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You  continue!');
+        // alert('You  continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Yura Shalya',
+                adress: {
+                    street: 'Teststreet',
+                    zipCode: '61000',
+                    country: 'UA'
+                },
+                email: 'testing@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => console.log(error));
     }
 
     render() {
