435f9b345f84f8acc6f0c7d6dda49b73ea5c59c8
Yura Shalya <shalyayura@gmail.com>
2019-02-05 01:04:08 +0200

    Settings Roure path

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const app = ( 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( app, document.getElementById('root'));
 
 
 
